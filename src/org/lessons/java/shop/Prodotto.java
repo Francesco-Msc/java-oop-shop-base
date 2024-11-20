9f0df428515a5a8e03bacf47ab90df1152ce6f49
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-20 18:44:38 +0100

    added a method to display the price excluding iva and implemented random number generation for the product code.

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,17 +1,24 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
     int codice;
     String nome;
     String descrizione;
-    String prezzo;
+    int prezzo;
     int iva;
 
-    public Prodotto(int codice, String nome, String descrizione, String prezzo, int iva){
-        this.codice = codice;
+    public Prodotto(int codice, String nome, String descrizione, int prezzo, int iva){
+        Random numeroRandom = new Random();
+        this.codice = numeroRandom.nextInt(10000);
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
         this.iva = iva;
+    }
+
+    public int prezzoIntero(){
+        return this.prezzo;
     }
 }
